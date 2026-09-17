4faee698a79c3c9563a0cbbea3ca5bd6e477e79c
Pengyang-uestc <2097586662@qq.com>
2026-09-17 15:40:22 +0800

    Update MyFrame.h

---
+++ MyEDA/src/MyFrame.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <wx/wx.h>
+
+// ============ 菜单 ID ============
+enum {
+    ID_MENU_NEW = wxID_HIGHEST + 1,
+    ID_MENU_OPEN,
+    ID_MENU_SAVE,
+    ID_MENU_SAVE_AS,
+    ID_MENU_EXPORT_NETLIST,
+    ID_MENU_IMPORT_NETLIST,
+    ID_MENU_UNDO,
+    ID_MENU_REDO,
+    ID_MENU_CUT,
+    ID_MENU_COPY,
+    ID_MENU_PASTE,
+    ID_MENU_DELETE,
+    ID_MENU_SIMULATE_START,
+    ID_MENU_SIMULATE_STOP,
+    ID_MENU_ABOUT,
+};
+
+// ============ 工具栏 ID ============
+enum {
+    ID_TOOL_SELECT = wxID_HIGHEST + 100,
+    ID_TOOL_WIRE,
+    ID_TOOL_DELETE,
+    ID_TOOL_AND,
+    ID_TOOL_OR,
+    ID_TOOL_NOT,
+    ID_TOOL_SIMULATE,
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame(const wxString& title);
+
+private:
+    // 创建函数
+    void CreateMenuBar();
+    void CreateToolBar();
+    void CreateStatusBar();
+
+    // 事件处理函数
+    void OnNew(wxCommandEvent& event);
+    void OnOpen(wxCommandEvent& event);
+    void OnSave(wxCommandEvent& event);
+    void OnSaveAs(wxCommandEvent& event);
+    void OnExportNetlist(wxCommandEvent& event);
+    void OnImportNetlist(wxCommandEvent& event);
+    void OnUndo(wxCommandEvent& event);
+    void OnRedo(wxCommandEvent& event);
+    void OnCut(wxCommandEvent& event);
+    void OnCopy(wxCommandEvent& event);
+    void OnPaste(wxCommandEvent& event);
+    void OnDelete(wxCommandEvent& event);
+    void OnSimulateStart(wxCommandEvent& event);
+    void OnSimulateStop(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+
+    // 工具栏事件
+    void OnToolSelect(wxCommandEvent& event);
+    void OnToolWire(wxCommandEvent& event);
+    void OnToolDelete(wxCommandEvent& event);
+    void OnToolAnd(wxCommandEvent& event);
+    void OnToolOr(wxCommandEvent& event);
+    void OnToolNot(wxCommandEvent& event);
+    void OnToolSimulate(wxCommandEvent& event);
+};
